b66de780d7e9688639a725455adfd55b401c968f
pachecomateus <mateuspachecosete@gmail.com>
2026-01-15 16:55:57 -0300

    fix: grammar errors

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -9,9 +9,7 @@ export default function WhoWeAre() {
         <p className="lg:text-xl font-light max-w-4xl mx-auto text-white text-justify">
           Somos movidos pela busca constante de soluções jurídicas que não apenas atendam às necessidades imediatas, mas que antecipem desafios e criem valor sustentável para nossos clientes. Aliamos profundidade técnica, pensamento inovador e proximidade no relacionamento para oferecer uma experiência de serviço diferenciada, que transforma riscos complexos em oportunidades de crescimento.
           <br /><br />
-          Unimos experiência prática, espírito crítico e inquietude criativa para ir além do óbvio. Não repetimos fórmulas prontas: analisamos com rigor, questionamos pressupostos, cruzamos dados e pensamos estrategicamente para entregar soluções sob medida que geram impacto real na prática do cliente.
-          <br /><br />
-          Entendemos cada desafio como uma oportunidade de excelência, com foco no cliente, precisão técnica e integridade profissional, guiando decisões que agregam valor jurídico e fortalecem a trajetória de quem confia em nosso trabalho.
+          Unimos experiência, espírito crítico e inquietude criativa para ir além do óbvio. Não repetimos fórmulas prontas: analisamos com rigor, questionamos pressupostos, cruzamos dados e pensamos estrategicamente para entregar soluções sob medida que geram impacto real na prática do cliente.
         </p>
       </div>
     </section>
